7d1d9a35332027631c0c51a4809b473fbd4cd330
ivandzf <divanandazf@gmail.com>
2018-10-12 11:08:48 +0700

    initial commit

---
+++ src/main/java/com/github/ivandzf/springbootangularexample/Application.java
@@ -0,0 +1,21 @@
+package com.github.ivandzf.springbootangularexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * springboot-angular-example
+ *
+ * @author Divananda Zikry Fadilla (12 October 2018)
+ * Email: divanandazf@gmail.com
+ * <p>
+ * Documentation here !!
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
